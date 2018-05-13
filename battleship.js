d7c6b535f399bda24a87a76d65b5206792dbc6fd
alprak <you@example.com>
2018-05-13 11:43:16 +0700

    done

--- battleship.js
+++ battleship.js
@@ -13,23 +13,25 @@ var store='';
 	store='';	
 	}
 
-	multiDim[0][1] = "@"
-	multiDim[0][2] = "@"
-	multiDim[0][3] = "@"
-	multiDim[0][4] = "@"
-	multiDim[4][1] = "@"
-	multiDim[5][1] = "@"
-	multiDim[6][1] = "@"
+	generateShip(multiDim,"=",5) // aircraft
+	generateShip(multiDim,"+",4) // battleship
+	generateShip(multiDim,"@",3) // cruiser
+	generateShip(multiDim,"*",2) // destroyer
 
-	//===================AIRCRAFT CARRIER========================
+
+return multiDim	
+}
+
+
+function generateShip(multiDim,shipSymbol,shipAdd) {
 	//==horizontal==
 	var countHrz=0
-	while(countHrz<=5) {
+	while(countHrz<=shipAdd) {
 		randomPos=[Math.floor(Math.random()*10),Math.floor(Math.random()*10)]
 		var tmpCoorXhrz=[]
 		var tmpCoorYhrz=[]
 		//console.log("xxxx")
-		for(let j=0;j<5;j++) {
+		for(let j=0;j<shipAdd;j++) {
 			//console.log(randomPos[0]+j,randomPos[1])
 			if(randomPos[0]+j <=9 && randomPos[0]+j >=0 && randomPos[1]<=9 && randomPos[1]>=0 && multiDim[randomPos[0]+j][randomPos[1]] == " ") {
 					tmpCoorXhrz.push(randomPos[0]+j)
@@ -41,20 +43,15 @@ var store='';
 			}	
 		}	
 	}
-
-	// for(let i=0;i<tmpCoorXhrz.length;i++) {
-	// 	multiDim[tmpCoorXhrz[i]][tmpCoorYhrz[i]] = "x"
-	// }
-
 	//==vertikal==
 
 	var countVer=0
-	while(countVer<=5) {
+	while(countVer<=shipAdd) {
 		randomPos=[Math.floor(Math.random()*10),Math.floor(Math.random()*10)]
 		var tmpCoorXver=[]
 		var tmpCoorYver=[]
 		//console.log("xxxx")
-		for(let j=0;j<5;j++) {
+		for(let j=0;j<shipAdd;j++) {
 			//console.log(randomPos[0]+j,randomPos[1])
 			if(randomPos[0] <=9 && randomPos[0] >=0 && randomPos[1]+j<=9 && randomPos[1]+j>=0 && multiDim[randomPos[0]][randomPos[1]+j] == " ") {
 					tmpCoorXver.push(randomPos[0])
@@ -66,35 +63,96 @@ var store='';
 			}	
 		}	
 	}
-
-	// for(let i=0;i<tmpCoorXver.length;i++) {
-	// 	multiDim[tmpCoorXver[i]][tmpCoorYver[i]] = "x"
-	// }
-
-
 	//shufling
 	
 	var probRand=Math.round(Math.random())
 
 	if(probRand == 0) {
 		for(let i=0;i<tmpCoorXhrz.length;i++) {		
-			multiDim[tmpCoorXhrz[i]][tmpCoorYhrz[i]] = "x"
+			multiDim[tmpCoorXhrz[i]][tmpCoorYhrz[i]] = shipSymbol
 		}	
 	}else {
 		for(let i=0;i<tmpCoorXver.length;i++) {
-			multiDim[tmpCoorXver[i]][tmpCoorYver[i]] = "x"
+			multiDim[tmpCoorXver[i]][tmpCoorYver[i]] = shipSymbol
 		}
 	}
-	
-		
-return multiDim	
+
+	//return multiDim
 }
 
-function generateShip() {
 
 
-	
+function shotShip(generateBoard,coordinat) {
+ var shotBoard = generateBoard
+ var aircraft = 5 // =
+ var battleship = 4 // +
+ var cruiser = 3 // @
+ var destroyer = 2 // *
+ for(let i=0;i<coordinat.length;i++) {
+ 	if(shotBoard[coordinat[i][0]][coordinat[i][1]] == ' ') {
+ 		
+ 		shotBoard[coordinat[i][0]][coordinat[i][1]] = '/'
+ 	}else {
+ 		if(shotBoard[coordinat[i][0]][coordinat[i][1]] == "="){
+ 			shotBoard[coordinat[i][0]][coordinat[i][1]] = 'X'
+ 			aircraft--
+ 			console.log(`aircraft tertembak, sisa kapal ${aircraft}`)	
+ 		}
+
+ 		if(shotBoard[coordinat[i][0]][coordinat[i][1]] == "+"){
+ 			shotBoard[coordinat[i][0]][coordinat[i][1]] = 'X'
+ 			battleship--
+ 			console.log(`battleship tertembak, sisa kapal ${battleship}`)	
+ 		}
+
+ 		if(shotBoard[coordinat[i][0]][coordinat[i][1]] == "@"){
+ 			shotBoard[coordinat[i][0]][coordinat[i][1]] = 'X'
+ 			cruiser--
+ 			console.log(`cruiser tertembak, sisa kapal ${cruiser}`)	
+ 		}
+
+ 		if(shotBoard[coordinat[i][0]][coordinat[i][1]] == "*"){
+ 			shotBoard[coordinat[i][0]][coordinat[i][1]] = 'X'
+ 			destroyer--
+ 			console.log(`destroyer tertembak, sisa kapal ${destroyer}`)	
+ 		}
+ 		
+ 	}
+ }
+
+ if(aircraft == 0 && battleship == 0 && cruiser ==0 && destroyer ==0) {
+ 	console.log("selamat anda menang!")
+ }
+
+//shotBoard[0][1]="X"
+
+return shotBoard
 } 
 
-console.log(generateBoard());
-var multiDim = generateBoard()
+var argv = process.argv
+var shot = argv.slice(2)
+var board = generateBoard()
+
+
+if(argv[2] == undefined) {
+	console.log("isi koordinat terlebih dahulu dengan contoh sbb:")
+	console.log(`00 --> baris 0,kolom 0`)
+	console.log(`31 --> baris 3, kolom 1`)
+	console.log(`51 --> baris 5 kolom 1`)
+	console.log(`note: maksimal baris dan kolom = 9, minimal baris dan kolom = 0 `)
+	console.log(board)
+}else{
+    console.log(board);
+	console.log("==============TEMBAKK!!=================")
+	var shotBoard = shotShip(board,shot)
+	console.log(shotBoard)
+}
+
+
+
+
+
+
+
+
+
